5f52fe2615f1d5835b3d78ac8fdce80bde601bfb
Roland Vachter <roland.vachter@iquestgroup.com>
2016-09-20 13:07:14 +0300

    Include o-overlay.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ exports['o-grid'] = require('o-grid');
 exports['o-header'] = require('o-header');
 exports['o-footer'] = require('o-footer');
 exports['o-tracking'] = require('o-tracking');
+exports['o-overlay'] = require('o-overlay');
 
 exports['header'] = require('./header/main');
 exports['marketslive-session-listener'] = require('./marketslive-session-listener/main');
