196482744d8bb70c72049c39dd47ee523d333dc8
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2018-06-25 17:13:56 +0300

    Update o-cookie-message to latest release

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ exports['o-footer'] = require('o-footer');
 exports['o-tracking'] = require('o-tracking');
 exports['o-overlay'] = require('o-overlay');
 exports['o-share'] = require('o-share');
+exports['o-cookie-message'] = require('o-cookie-message');
 
 exports['header'] = require('./header/main');
 exports['marketslive-session-listener'] = require('./marketslive-session-listener/main');
 exports['marketslive-session-notification'] = require('./marketslive-session-notification/main');
 exports['tracking'] = require('./tracking/main');
 exports['InfiniteScroll'] = require('./infinite-scroll/main');
-exports['cookieBanner'] = require('./cookie-banner/main');
 
 exports['AlertOverlay'] = require('./overlays/AlertOverlay');
 exports['ConfirmOverlay'] = require('./overlays/ConfirmOverlay');
