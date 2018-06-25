cb02b6eed0b891a81a8c0ad9a5bb4f9cd863ae3e
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2018-06-25 12:24:00 +0300

    Pass the cookie banner component explicitly

--- cookie-banner/main.js
+++ cookie-banner/main.js
@@ -2,8 +2,9 @@ const oCookieMessage = require('o-cookie-message');
 
 document.addEventListener("o.DOMContentLoaded", function() {
 	const redirect = window.location.href;
+	const component = document.querySelector('[data-o-component="o-cookie-message"]');
 
-	const cookieMessage = new oCookieMessage(null, {
+	const cookieMessage = new oCookieMessage(component, {
 		acceptUrl: `https://consent.ft.com/__consent/consent-record-cookie`,
 		acceptUrlFallback: `https://consent.ft.com/__consent/consent-record-cookie?redirect=${redirect}&cookieDomain=.ft.com`,
 	});
