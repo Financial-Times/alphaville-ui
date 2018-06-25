3697e7d9683953407e427c2b712547be28d6a633
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2018-06-25 11:21:45 +0300

    Fix cookie banner urls

--- cookie-banner/main.js
+++ cookie-banner/main.js
@@ -1,3 +1,10 @@
 const oCookieMessage = require('o-cookie-message');
 
-const cookieMessage = new oCookieMessage();
+document.addEventListener("o.DOMContentLoaded", function() {
+	const redirect = window.location.href;
+
+	const cookieMessage = new oCookieMessage(null, {
+		acceptUrl: `https://consent.ft.com/__consent/consent-record-cookie`,
+		acceptUrlFallback: `https://consent.ft.com/__consent/consent-record-cookie?redirect=${redirect}&cookieDomain=.ft.com`,
+	});
+});
